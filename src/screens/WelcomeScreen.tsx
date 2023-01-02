267d839aca2b29cf736754d78cb0569e25162f42
jonnyabrams <jonnyabrams@hotmail.com>
2023-01-02 15:53:45 +0000

    design WelcomeScreen with logo & buttons

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,4 +1,4 @@
-import { ImageBackground, StyleSheet } from "react-native";
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const WelcomeScreen = () => {
@@ -6,7 +6,18 @@ const WelcomeScreen = () => {
     <ImageBackground
       style={styles.background}
       source={require("../assets/background.jpg")}
-    ></ImageBackground>
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/u-logo.png")} />
+        <Text>Ditch Your Old Crap With This Snazzy New App!</Text>
+      </View>
+      <View style={styles.loginButton}>
+        <Text style={styles.buttonText}>Log In</Text>
+      </View>
+      <View style={styles.registerButton}>
+        <Text style={styles.buttonText}>Register</Text>
+      </View>
+    </ImageBackground>
   );
 };
 
@@ -14,6 +25,35 @@ export default WelcomeScreen;
 
 const styles = StyleSheet.create({
   background: {
-    flex: 1
-  }
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+    justifyContent: "center",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+    justifyContent: "center",
+  },
 });
