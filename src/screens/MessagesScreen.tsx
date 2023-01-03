83c019a9a484090693260788a50ab58352c5398b
jonnyabrams <jonnyabrams@hotmail.com>
2023-01-03 17:47:33 +0000

    refactor into ListItemDeleteAction component

--- src/screens/MessagesScreen.tsx
+++ src/screens/MessagesScreen.tsx
@@ -2,6 +2,7 @@ import { StyleSheet, View } from "react-native";
 import { FlatList } from "react-native";
 
 import ListItem from "../components/ListItem";
+import ListItemDeleteAction from "../components/ListItemDeleteAction";
 import ListItemSeparator from "../components/ListItemSeparator";
 import Screen from "../components/Screen";
 
@@ -43,11 +44,8 @@ const MessagesScreen = () => {
             title={item.title}
             subtitle={item.description}
             image={item.image}
-            onPress={() => alert(item.id)}
-            renderRightActions={() => <View style={{
-              backgroundColor: 'red',
-              width: 70
-            }}></View>}
+            onPress={() => console.log(item.id)}
+            renderRightActions={ListItemDeleteAction}
           />
         )}
         ItemSeparatorComponent={() => <ListItemSeparator />}
