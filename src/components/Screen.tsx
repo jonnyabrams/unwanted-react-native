d87b82d3c893191c8b2800872380366350e4dff0
jonnyabrams <jonnyabrams@hotmail.com>
2023-01-06 16:53:25 +0000

    fix pull on refresh cutoff problem again (had returned due to changes)

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -9,7 +9,7 @@ interface IProps {
 const Screen = ({ children, style }: IProps) => {
   return (
     <SafeAreaView style={{ ...styles.screen, ...style }}>
-      <View style={styles.content}>{children}</View>
+      <View style={{ ...styles.view, ...style }}>{children}</View>
     </SafeAreaView>
   );
 };
@@ -17,8 +17,9 @@ const Screen = ({ children, style }: IProps) => {
 export default Screen;
 
 const styles = StyleSheet.create({
-  content: {
-    padding: 20
+  view: {
+    padding: 20,
+    flex: 1,
   },
   screen: {
     paddingTop: Constants.statusBarHeight,
