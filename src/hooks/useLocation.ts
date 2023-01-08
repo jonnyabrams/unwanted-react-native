7e8958eb0438501b5517562bfb5e37146feb3141
jonnyabrams <jonnyabrams@hotmail.com>
2023-01-08 14:33:40 +0000

    refactor useLocation hook to just return long and lat

--- src/hooks/useLocation.ts
+++ src/hooks/useLocation.ts
@@ -3,34 +3,26 @@ import { LocationObject } from "expo-location";
 import { useEffect, useState } from "react";
 
 const useLocation = () => {
-  const [location, setLocation] = useState<LocationObject | null>(null);
-  const [errorMsg, setErrorMsg] = useState("");
+  const [location, setLocation] = useState<LocationType | null>(null);
 
-  try {
-    useEffect(() => {
-      (async () => {
-        let { status } = await Location.requestForegroundPermissionsAsync();
-        if (status !== "granted") {
-          setErrorMsg("Permission to access location was denied");
-          return;
-        }
-  
-        let location = await Location.getCurrentPositionAsync({});
-        setLocation(location);
-      })();
-    }, []);
-  
-    // let text = "Waiting..";
-    // if (errorMsg) {
-    //   text = errorMsg;
-    // } else if (location) {
-    //   text = JSON.stringify(location);
-    // }
-  
-    return location;
-  } catch (error) {
-    console.log(error, errorMsg);
-  }
+  const getLocation = async () => {
+    try {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") return;
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      setLocation({ latitude, longitude });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  return location;
 };
 
 export default useLocation;
