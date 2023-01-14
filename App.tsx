d8421c6aa9c9998acc4f202f868f1bc71474f726
jonnyabrams <jonnyabrams@hotmail.com>
2023-01-14 09:42:16 +0000

    refactor decode token logic out of App.tsx into storage

--- App.tsx
+++ App.tsx
@@ -1,25 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useEffect, useState } from "react";
-import jwtDecode from "jwt-decode";
 
-import AppPicker from "./src/components/Picker";
-import AppTextInput from "./src/components/TextInput";
-import Icon from "./src/components/Icon";
-import ListItem from "./src/components/ListItem";
-import Screen from "./src/components/Screen";
-import AccountScreen from "./src/screens/AccountScreen";
-import ListingDetailsScreen from "./src/screens/ListingDetailsScreen";
-import ListingEditScreen from "./src/screens/ListingEditScreen";
-import ListingsScreen from "./src/screens/ListingsScreen";
-import LoginScreen from "./src/screens/LoginScreen";
-import MessagesScreen from "./src/screens/MessagesScreen";
-import RegisterScreen from "./src/screens/RegisterScreen";
-import ViewImageScreen from "./src/screens/ViewImageScreen";
-import WelcomeScreen from "./src/screens/WelcomeScreen";
-import Button from "./src/components/Button";
-import ImageInput from "./src/components/ImageInput";
-import ImageInputList from "./src/components/ImageInputList";
 import AuthNavigator from "./src/navigation/AuthNavigator";
 import navigationTheme from "./src/navigation/navigationTheme";
 import AppNavigator from "./src/navigation/AppNavigator";
@@ -31,13 +12,12 @@ const App = () => {
   const [user, setUser] = useState<UserType | null>(null);
 
   useEffect(() => {
-    const restoreToken = async () => {
-      const token = await authStorage.getToken();
-      if (!token) return;
-      setUser(jwtDecode(token));
+    const restoreUser = async () => {
+      const user = await authStorage.getUser();
+      if (user) setUser(user as UserType);
     };
 
-    restoreToken();
+    restoreUser();
   }, []);
 
   return (
