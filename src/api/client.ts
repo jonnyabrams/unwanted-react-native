d03b4534e928866a9eb7980c05a1f5737a27067a
jonnyabrams <jonnyabrams@hotmail.com>
2023-01-14 11:39:41 +0000

    include auth token in headers for protected routes

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,15 @@
 import { create } from "apisauce";
 
+import authStorage from "../auth/storage";
+
 const apiClient = create({
   baseURL: "http://localhost:9000/api",
+});
+
+apiClient.addAsyncRequestTransform(async (req) => {
+  const authToken = await authStorage.getToken();
+  if (!authToken) return;
+  req.headers["x-auth-token"] = authToken;
 });
 
 export default apiClient;
